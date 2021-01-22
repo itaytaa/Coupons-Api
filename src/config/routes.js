799ff003b1d2324e295caaf0a1c2fccda385c20b
itay taasa <itaytaasa@gmail.com>
2021-01-22 12:37:17 +0200

    fixed synthax error

--- src/config/routes.js
+++ src/config/routes.js
@@ -10,6 +10,6 @@ routes.get('/coupon/:id',Coupons.get)
 routes.post('/Coupon/:id',Coupons.update)
 routes.post('/Coupon/:id/redeem',Coupons.redeem)
 routes.delete('/Coupon/:id',Coupons.delete)
-routes.delete('/Coupon/:id',Coupons.isExist)
+routes.get('/Coupon/:id',Coupons.isExist)
 
 module.exports = routes;
